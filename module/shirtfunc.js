2470178b414cfad9d1d6bfedbd32a9fceaa7fade
wzwxwc <zc_1987>
2016-07-14 21:38:55 +0800

    整理目录结构

--- module/shirtfunc.js
+++ module/shirtfunc.js
@@ -2,9 +2,10 @@
  * Created by zc on 2016/7/4.
  */
 define(function () {
-    //do something here
+    var color = "yellow";
+    var size = 16;
     return {
-        color: "black",
-        size: "unsize"
+        color: color,
+        size: size
     };
 });
