04f4e3e853f22c4a8709aab014c17275dd94c8ec
wzwxwc <475977851@qq.com>
2016-07-22 15:51:11 +0800

    no comments from zcG460

---
+++ codes/02为什么要这样引入jquery/main3.js
@@ -0,0 +1,16 @@
+/**
+ * Created by zc on 2016/7/1.
+ */
+require.config({
+    baseUrl: '/requireJsStudy',
+    paths: {
+        jquery: "/deps/jquery-3.0.0"
+    }
+});
+
+require(['jquery', 'lib/lib'], function (b, lib) {
+    b("#mydiv").css('background-color', 'yellow');
+    var a = new lib();
+    // a.say();
+
+})
