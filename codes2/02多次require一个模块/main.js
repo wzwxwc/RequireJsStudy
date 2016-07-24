361402aaa64dbe214d3655be7b899266b40b04f7
wzwxwc <475977851@qq.com>
2016-07-24 18:22:29 +0800

    no comments from zcG460

--- codes2/02多次require一个模块/main.js
+++ codes2/02多次require一个模块/main.js
@@ -4,6 +4,6 @@
 require.config({
     baseurl: "/requireJsStudy/codes2/02多次require一个模块"
 });
-require(["module1", "module2"], function () {
+require(["module1", "module2", "module3"], function () {
     console.log("我就想试验一下，那个requiredModule是否会被多次请求？");
 });
