0ee12ee1813d58a511ce093a401c5bbff1a3fe1b
wzwxwc <zc_1987>
2017-05-09 10:45:12 +0800

    no comments from zcRescuer

---
+++ codes1/03打包/scripts/main-built.js
@@ -0,0 +1,3 @@
+/**
+ * Created by zcRescuer on 2017/5/9.
+ */
---
+++ codes1/03打包/scripts/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by zcRescuer on 2017/5/9.
+ */
+
+require(["one", "two", "three"], function (one, two, three) {
+    alert(one+two+three)
+});
