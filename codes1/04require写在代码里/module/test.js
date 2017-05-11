f5eb77b78ddb4f432225fd5e33c6e657c11e54df
wzwxwc <zc_1987>
2017-05-11 14:35:25 +0800

    no comments from zcRescuer

--- codes1/04require写在代码里/module/test.js
+++ codes1/04require写在代码里/module/test.js
@@ -5,13 +5,12 @@ require.config({
     paths: {
         aa: "./a"
     }
-})
-define([], function () {
+});
+require([], function () {
     var flag = true;
     if (flag) {
         //下述这种用法是行不通的！！
-        var aa = require("aa");
-        // console.log(aa);
+        require("./a")
     } else {
         require("./b");
     }
