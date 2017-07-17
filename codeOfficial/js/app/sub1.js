b0f48acb686d81a70f38cf9aa95e2ac23b41de89
wzwxwc <zc_1987>
2017-07-17 16:01:38 +0800

    no comments from zcRescuer

---
+++ codeOfficial/js/app/sub1.js
@@ -0,0 +1,6 @@
+/**
+ * Created by zc on 2017/7/14.
+ */
+define(["jquery"], function ($) {
+    console.log("sub1被加载");
+});
---
+++ codeOfficial/js/collection/module1/module1.js
@@ -0,0 +1,6 @@
+/**
+ * Created by zc on 2017/7/14.
+ */
+define(["collection/module2"], function () {
+    console.log("模块1被加载");
+});
