c19026bd5c2648d7184ea5578bf3b9fb1ccf01c7
wzwxwc <475977851@qq.com>
2017-10-23 17:18:43 +0800

    no comments from zcRescuer

---
+++ codes2/05主页只有一个入口/module1.js
@@ -0,0 +1,5 @@
+define([], function () {
+    var temp={}
+    temp.name="module1";
+    return temp;
+});
---
+++ codes2/05主页只有一个入口/module2.js
@@ -0,0 +1,5 @@
+define([], function () {
+    var temp={}
+    temp.name="module2";
+    return temp;
+});
